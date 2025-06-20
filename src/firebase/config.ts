852586d7d381ec320ed40a700ef22738d59b926f
DaudRazzaq <139013552+DaudRazzaq@users.noreply.github.com>
2025-06-20 15:27:43 +0500

    push

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- 
+ apiKey: "AIzaSyAgPvTH-0FDbFYIstOpyd9l_2lym-ap0xk",
+  authDomain: "interview-c67e0.firebaseapp.com",
+  projectId: "interview-c67e0",
+  storageBucket: "interview-c67e0.firebasestorage.app",
+  messagingSenderId: "690683423646",
+  appId: "1:690683423646:web:a2db93320b752a3141848f",
+  measurementId: "G-NWCJFLYB0B"
 };
 
 // Initialize Firebase
